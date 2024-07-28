028ba6d78f4bec476f763fa78f7aa1b80f96cb24
Martin Bulant <Martin.Bulant@seznam.cz>
2024-07-28 18:23:21 +0200

    just small edit of the test runner

--- runTest.bat
+++ runTest.bat
@@ -1,10 +1,28 @@
 @echo off
 
 echo  Test - Runnig 
-python -m unittest
+
+IF "%1"=="-v" (
+    call :verboseMode
+) ELSE (
+    call :classicMode
+)
+
+
 
 IF %ERRORLEVEL% EQU 0 (
     echo Test - Done
 ) ELSE (
     echo Something went wrong.
 )
+
+
+
+exit /b 0
+:classicMode
+    python -m unittest
+    exit /b 0
+
+:verboseMode
+    python -m unittest -v
+    exit /b 0
